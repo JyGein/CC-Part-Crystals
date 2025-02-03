85e909eaff7ad3059ce91f4083dbc91a609da531
JyGein <52318346+JyGein@users.noreply.github.com>
2025-02-03 15:30:58 -0500

    weird gitignore

---
+++ Artifacts/CraftArtifact.cs
@@ -0,0 +1,23 @@
+﻿using JyGein.PartCrystals;
+using System.Collections.Generic;
+
+namespace JyGein.PartCrystals.Artifacts;
+
+internal sealed class CraftArtifact : Artifact
+{
+    public int CraftsLeft = ModEntry.Instance.Settings.ProfileBased.Current.InitialCrafts;
+
+    public override int? GetDisplayNumber(State s)
+        => CraftsLeft;
+
+    public override List<Tooltip>? GetExtraTooltips()
+        => [
+            .. base.GetExtraTooltips() ?? [],
+            new TTText(ModEntry.Instance.Localizations.Localize(["artifact", "extraDescription", CraftsLeft switch
+            {
+                0 => "zero",
+                1 => "one",
+                _ => "other"
+            }], new { Amount = CraftsLeft }))
+        ];
+}
